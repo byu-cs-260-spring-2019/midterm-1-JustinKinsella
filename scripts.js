a676ba132545a5a12b791fcdba91a4ffcf8d5596
Justin Kinsella <justin.d.kinsella@gmail.com>
2019-05-23 13:49:54 -0600

    now retrieving Json to current object

--- scripts.js
+++ scripts.js
@@ -2,11 +2,41 @@ var app = new Vue({
     el: "#app",
     data:{
         search: '',
+        searchValue: '',
+        loading: true,
+        current: {},
 
     },
-    methods:{
-        submit() {
 
-        },
+
+    mounted() {
+        this.submit();
+    },
+
+
+    methods:{
+        async submit() {
+            let searchVal = this.search;
+            while(searchVal.search(' ') !== -1 ){
+                searchVal = searchVal.replace(' ', "+");
+                this.searchValue = searchVal;
+            }
+
+            try {
+                this.loading = true;
+                const response = await axios.get( 'http://openlibrary.org/search.json?q=' + this.search);
+                this.current = response.data;
+                this.loading = false;
+                console.log("response:  ", response.data);
+                return true;
+
+            }
+            catch(error) {
+                console.log(error);
+            }
+        },           
+    
     }
+
+
 });
